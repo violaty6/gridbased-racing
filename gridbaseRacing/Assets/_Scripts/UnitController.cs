1ada9a04709ffbbba52384cfc33445f280daca1a
violaty6 <violaty9510@gmail.com>
2024-03-13 09:53:28 +0300

    Movement Code Improve 2

--- gridbaseRacing/Assets/_Scripts/UnitController.cs
+++ gridbaseRacing/Assets/_Scripts/UnitController.cs
@@ -9,6 +9,7 @@ using UnityEngine.InputSystem;
 
 public class UnitController : MonoBehaviour
 {
+    [SerializeField]private bool isCrashed = false;
     [SerializeField] private GridManager _gridManager;
     [SerializeField] private Node currentNode;
     [SerializeField] private int _UnitEnginePower;
@@ -51,6 +52,7 @@ public class UnitController : MonoBehaviour
     {
         Vector2 input = ctx.ReadValue<Vector2>();
         Vector3 moveDirection = Vector3.zero;
+        Vector3Int targetGrid = new Vector3Int(0,0,0);
         List<Node> checkNodes = new List<Node>();
         List<Node.NodeType> checkNodeType = new List<Node.NodeType>();
         for (int i = 1; i <= _UnitEnginePower; i++)
@@ -66,17 +68,43 @@ public class UnitController : MonoBehaviour
             Vector3Int moveDirectionInt = new Vector3Int(Mathf.RoundToInt(moveDirection.x), 0, Mathf.RoundToInt(moveDirection.z));
             Vector3Int targetCord = currentNode.cords + moveDirectionInt;
             Node targetNode = _gridManager.GetTileAt(targetCord);
-            checkNodes.Add(targetNode);
-            if (targetNode==null) checkNodeType.Add(Node.NodeType.Void);
-            else checkNodeType.Add(targetNode.currentType);
+            targetGrid = targetCord;
+            CheckNode(targetNode,checkNodes, checkNodeType);
         }
+        CheckAndMove(checkNodes, checkNodeType,targetGrid);
+    }
+    private void CheckNode(Node targetNode,List<Node> checkNodes,List<Node.NodeType> checkNodeType)
+    {
+        checkNodes.Add(targetNode);
+        if (targetNode == null)
+        {
+            checkNodeType.Add(Node.NodeType.Void);
+        }
+        else
+        {
+            checkNodeType.Add(targetNode.currentType);
+        }
+    }
+
+    private void CheckAndMove(List<Node> checkNodes,List<Node.NodeType> checkNodeType,Vector3Int targetGrid)
+    {
         if (!checkNodes.Contains(null) && !checkNodeType.Contains(Node.NodeType.Obstacle) && !checkNodeType.Contains(Node.NodeType.Void))
         {
             MoveFeedBack(checkNodes.Last());
             currentNode = checkNodes.Last();
         }
+        else
+        {
+            isCrashed = true;
+        }
+    }
+    void CrashFeedback(Vector3Int crashGridCords)
+    {
+        // transform.DOMove(targetNode.cords, 1.25f).SetEase(Ease.OutQuart);
+        // transform.DOLookAt(targetNode.cords, 0.1f);
     }
 
+    
     void MoveFeedBack(Node targetNode)
     {
         transform.DOMove(targetNode.cords, 1.25f).SetEase(Ease.OutQuart);
